5227073891f2f068f07d786adf080d78b88af91f
waverDeep <dandyleader@naver.com>
2020-10-04 13:07:58 +0900

    텍스트 추출 코드 작성

--- playground.py
+++ playground.py
@@ -1,7 +1,10 @@
 import fileOI
-
+import script_manipulate
 
 dev_dataset = './dataset/dev.trn'
 data_list = fileOI.read_txt_file(dev_dataset)
 
 print(data_list[0])
+
+_, script_lines = script_manipulate.get_only_script(data_list)
+print(script_lines[0])
