cb71feb23f7755ba1c53b2a8eaf1ed4584c09a08
waverDeep <dandyleader@naver.com>
2020-10-05 15:49:13 +0900

    스크립트에 표현된 감탄사 제거 코드 작성

--- script_manipulate.py
+++ script_manipulate.py
@@ -7,3 +7,21 @@ def get_only_script(lines, split_keyword='::'):
         script_lines.append(temp[1].strip()) # 앞뒤 공백 제거
 
     return path_lines, script_lines
+
+
+def remove_interjection(line, keyword='아/ '):
+    temp = line.replace(keyword, '')
+    temp = ' '.join(temp.split())
+    return temp
+
+
+def remove_interjections_in_list(lines, *args):
+    datasets = []
+    for line in lines:
+        temp = line
+        for arg in args:
+            temp = temp.replace(arg, '')
+        temp = ' '.join(temp.split())
+        datasets.append(temp)
+    return datasets
+
