26f714a7d4799d18047701355f5909c298eec8bb
waverDeep <dandyleader@naver.com>
2020-10-21 11:30:27 +0900

    json 형태의 파일을 학습 테스트 셋 분류 코드 작성

--- script_preprocess.py
+++ script_preprocess.py
@@ -2,9 +2,9 @@ from fileOI import filename_script_pair_tolist
 import script_manipulate
 from tqdm import tqdm
 import fileOI
+import random
 
-
-def merge_script_like_libris(file_list, divider=' ', encoding='euc-kr'):
+def merge_script_like_libris(file_list, divider=' ', encoding='utf8'):
     dataset = []
     # step 01 : 파일리스트를 통해서 스크립트 읽기
     for file in file_list:
@@ -32,26 +32,30 @@ def remove_options(line):
     return line
 
 
-def merge_script_like_clova_call(file_list, encoding='euc-kr'):
+def merge_script_like_clova_call(file_list, encoding='utf8'):
     dataset = []
     # step 01 : 파일리스트를 통해서 스크립트 읽기
     for file in tqdm(file_list):
         temp = filename_script_pair_tolist(file, encoding)
         # step 01 -01 : u/ 이 삽입된 문장은 삭제
-        if script_manipulate.is_remove_line(temp[1]):
-            break
-        temp[1] = remove_options(temp[1])
-        filename = temp[0]
-        text = temp[1]
-        speaker_id = 0
-        print(text)
-        dic_temp = {'wav': filename, 'text': text, 'speaker_id': speaker_id}
-        dataset.append(dic_temp)
+        if not script_manipulate.is_remove_line(temp[1]):
+            temp[1] = remove_options(temp[1])
+            filename = temp[0]
+            text = temp[1]
+            speaker_id = 0
+            dic_temp = {'wav': filename, 'text': text, 'speaker_id': speaker_id}
+            dataset.append(dic_temp)
     return dataset
 
 
-def split_train_test_dataset_with_json(filepath, split_rate=0.2, encoding='utf-8'):
-    json_data = fileOI.read_json_file(filepath, encoding=encoding)
+def split_train_test_dataset_with_json(dataset, split_rate=0.2, encoding='utf8'):
+    json_data = dataset
+    random.shuffle(json_data)
+    json_length = len(json_data)
+    test_length = int(json_length*split_rate)
+    testset = json_data[0:test_length]
+    trainset = json_data[test_length:]
+    return trainset, testset
 
 
 
